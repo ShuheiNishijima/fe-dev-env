78b72a1bdcbbb08ccdb8e0308d1e7360612fbcf5
shuhei.nishijima <nishijimasyuhei@gmail.com>
2019-10-13 15:09:47 +0900

    add setting file.

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry:  './js/src/index.js',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'js/dist')
+  },
+  devServer: {
+    open: true,
+    openPage: "index.html",
+    watchContentBase: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {loader: MiniCssExtractPlugin.loader,},
+          {loader: 'css-loader',},
+          {loader: 'postcss-loader',},
+          {loader: 'sass-loader',}
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '../../assets/style/css/style.css',
+    })
+  ]
+};
