c26b8b264e348b6004912eadcf73815cb05f7280
shuhei.nishijima <nishijimasyuhei@gmail.com>
2020-04-08 18:49:21 +0900

    change file.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry:  './js/src/index.js',
+  entry:  './src/index.js',
   output: {
     filename: 'main.bundle.js',
     path: path.resolve(__dirname, 'js/dist')
